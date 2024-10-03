2c3ade97fdd7da6a978d9bbfc8cdc596c8b776c5
TOKHIRJON TURGUNOV <myfuzei@gmail.com>
2024-10-04 06:45:11 +0900

    BRR: Initial Setup

---
+++ src/app/App.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import  App  from "../app/App";  // Named export from App.test.tsx
+
+
+test("renders learn react link", () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  expect(getByText(/learn/i)).toBeInTheDocument();
+});
+
+export default App;
